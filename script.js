0336537921777e61475cf07e96ef12820f3856c5
Jordan2807 <jordanr2807@gmail.com>
2026-03-24 12:53:11 -0600

    1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Contador para pasar la imagen del home
 window.addEventListener('load', () => {
     const imagenes = document.querySelectorAll('.hero-img');
     let index = 0;
@@ -7,4 +8,16 @@ window.addEventListener('load', () => {
         index = (index + 1) % imagenes.length;
         imagenes[index].classList.add('active');
     }, 4000); //cambia la imagen cada 4 segundos
+});
+
+//Ver contraseña
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", function () {
+
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    this.classList.toggle("fa-eye-slash");
 });
